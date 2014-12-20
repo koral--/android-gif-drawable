46771a85cd09780d2291a151d4edca104bf445ee
koral-- <koral--@users.noreply.github.com>
2014-12-20 03:08:36 +0100

    hiding compound drawables added

--- src/main/java/pl/droidsonroids/gif/GifTextView.java
+++ src/main/java/pl/droidsonroids/gif/GifTextView.java
@@ -23,6 +23,7 @@ public class GifTextView extends TextView {
 
     /**
      * A corresponding superclass constructor wrapper.
+     *
      * @param context
      */
     public GifTextView(Context context) {
@@ -32,9 +33,10 @@ public class GifTextView extends TextView {
     /**
      * Like equivalent from superclass but also try to interpret compound drawables defined in XML
      * attributes as GIFs.
-     * @see TextView#TextView(Context, AttributeSet)
+     *
      * @param context
      * @param attrs
+     * @see TextView#TextView(Context, AttributeSet)
      */
     public GifTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -44,10 +46,11 @@ public class GifTextView extends TextView {
     /**
      * Like equivalent from superclass but also try to interpret compound drawables defined in XML
      * attributes as GIFs.
-     * @see TextView#TextView(Context, AttributeSet, int)
+     *
      * @param context
      * @param attrs
      * @param defStyle
+     * @see TextView#TextView(Context, AttributeSet, int)
      */
     public GifTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -73,16 +76,12 @@ public class GifTextView extends TextView {
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     private void parseAttrs(AttributeSet attrs) {
         if (attrs != null) {
-            Drawable left = getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableLeft", 0)), right = getGifOrDefaultDrawable(attrs
-                    .getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableRight", 0)), top = getGifOrDefaultDrawable(attrs
-                    .getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableTop", 0)), bottom = getGifOrDefaultDrawable(attrs
-                    .getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableBottom", 0)), start = getGifOrDefaultDrawable(attrs
-                    .getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableStart", 0)), end = getGifOrDefaultDrawable(attrs
-                    .getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableEnd", 0));
+            Drawable left = getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableLeft", 0)), right = getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableRight", 0)), top = getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableTop", 0)), bottom = getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableBottom", 0)), start = getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableStart", 0)), end = getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS, "drawableEnd", 0));
             setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
                 setCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, bottom);
+            }
             setBackgroundInternal(getGifOrDefaultDrawable(attrs.getAttributeResourceValue(GifImageView.ANDROID_NS, "background", 0)));
         }
     }
@@ -92,10 +91,11 @@ public class GifTextView extends TextView {
     @SuppressWarnings("deprecation")
     // setBackgroundDrawable
     private void setBackgroundInternal(Drawable bg) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             setBackground(bg);
-        else
+        } else {
             setBackgroundDrawable(bg);
+        }
     }
 
     @Override
@@ -104,28 +104,45 @@ public class GifTextView extends TextView {
     }
 
     private Drawable getGifOrDefaultDrawable(int resId) {
-        if (resId == 0)
+        if (resId == 0) {
             return null;
+        }
         final Resources resources = getResources();
-        if (!isInEditMode() && "drawable".equals(resources.getResourceTypeName(resId)))
+        if (!isInEditMode() && "drawable".equals(resources.getResourceTypeName(resId))) {
             try {
                 return new GifDrawable(resources, resId);
             } catch (IOException | NotFoundException ignored) {
                 // ignored
             }
+        }
         return resources.getDrawable(resId);
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     public void setCompoundDrawablesRelativeWithIntrinsicBounds(int start, int top, int end, int bottom) {
-        setCompoundDrawablesRelativeWithIntrinsicBounds(getGifOrDefaultDrawable(start), getGifOrDefaultDrawable(top),
-                getGifOrDefaultDrawable(end), getGifOrDefaultDrawable(bottom));
+        setCompoundDrawablesRelativeWithIntrinsicBounds(getGifOrDefaultDrawable(start), getGifOrDefaultDrawable(top), getGifOrDefaultDrawable(end), getGifOrDefaultDrawable(bottom));
     }
 
     @Override
     public void setCompoundDrawablesWithIntrinsicBounds(int left, int top, int right, int bottom) {
-        setCompoundDrawablesWithIntrinsicBounds(getGifOrDefaultDrawable(left), getGifOrDefaultDrawable(top),
-                getGifOrDefaultDrawable(right), getGifOrDefaultDrawable(bottom));
+        setCompoundDrawablesWithIntrinsicBounds(getGifOrDefaultDrawable(left), getGifOrDefaultDrawable(top), getGifOrDefaultDrawable(right), getGifOrDefaultDrawable(bottom));
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        hideCompoundDrawables(getCompoundDrawables());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            hideCompoundDrawables(getCompoundDrawablesRelative());
+        }
+    }
+
+    private void hideCompoundDrawables(Drawable[] drawables) {
+        for (Drawable d : drawables) {
+            if (d != null) {
+                d.setVisible(false, false);
+            }
+        }
     }
 }
