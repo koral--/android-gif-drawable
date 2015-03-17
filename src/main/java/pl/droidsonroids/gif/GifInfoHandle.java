1e3d46911af487a0afabd84c79b5266f156223f4
Karol Wrótniak <koral--@users.noreply.github.com>
2015-03-17 02:26:22 +0100

    #128 code cleaning

--- src/main/java/pl/droidsonroids/gif/GifInfoHandle.java
+++ src/main/java/pl/droidsonroids/gif/GifInfoHandle.java
@@ -94,9 +94,9 @@ final class GifInfoHandle {
     }
 
     static GifInfoHandle openUri(ContentResolver resolver, Uri uri, boolean justDecodeMetaData) throws IOException {
-        if (ContentResolver.SCHEME_FILE.equals(uri.getScheme()))
-            return openAssetFileDescriptor(resolver.openAssetFileDescriptor(uri,"r"), justDecodeMetaData);
-        return openFile(uri.getPath(), justDecodeMetaData);
+        if (ContentResolver.SCHEME_FILE.equals(uri.getScheme())) //workaround for #128
+            return openFile(uri.getPath(), justDecodeMetaData);
+        return openAssetFileDescriptor(resolver.openAssetFileDescriptor(uri,"r"), justDecodeMetaData);
     }
 
     synchronized long renderFrame(Bitmap frameBuffer) {
