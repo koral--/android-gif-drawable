4111b17aa8e5644a81c6a459399ae10659bc7e19
Karol Wrótniak <koral--@users.noreply.github.com>
2015-04-01 03:32:37 +0200

    bound checking in seek fixed

--- src/main/jni/control.c
+++ src/main/jni/control.c
@@ -62,7 +62,7 @@ Java_pl_droidsonroids_gif_GifInfoHandle_seekToTime(JNIEnv *env, jclass __unused 
         if (lockPixels(env, jbitmap, info, &pixels) != 0) {
             return;
         }
-        while (info->currentIndex++ <= desiredIndex) {
+        while (info->currentIndex++ <= desiredIndex && info->currentIndex < info->gifFilePtr->ImageCount) {
             getBitmap((argb *) pixels, info);
         }
         unlockPixels(env, jbitmap);
@@ -99,7 +99,7 @@ Java_pl_droidsonroids_gif_GifInfoHandle_seekToFrame(JNIEnv *env, jclass __unused
     if (lockPixels(env, jbitmap, info, &pixels) != 0) {
         return;
     }
-    while (info->currentIndex++ < desiredIndex) {
+    while (info->currentIndex++ < desiredIndex && info->currentIndex < info->gifFilePtr->ImageCount) {
         getBitmap((argb *) pixels, info);
     }
     unlockPixels(env, jbitmap);
