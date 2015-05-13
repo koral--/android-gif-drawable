a4dc65d915dd1b3f9bfd4b0e53fe3cef9ce213b0
Karol Wrótniak <koral--@users.noreply.github.com>
2015-05-13 03:36:12 +0200

    Thread-safe MultiCallback for #135

--- src/main/java/pl/droidsonroids/gif/MultiCallback.java
+++ src/main/java/pl/droidsonroids/gif/MultiCallback.java
@@ -6,97 +6,96 @@ import android.support.annotation.NonNull;
 import android.view.View;
 
 import java.lang.ref.WeakReference;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * {@link Callback} which allows single {@link Drawable} to be associated with multiple {@link View}s.
  * If drawable needs to be redrawn all its views are invalidated.
+ *
  * @author koral--
  * @author Doctoror
  */
 public class MultiCallback implements Callback {
 
-    private final Set<ViewWeakReference> mViewSet = new LinkedHashSet<>();
+    private final CopyOnWriteArrayList<ViewWeakReference> mViewList = new CopyOnWriteArrayList<>();
 
     @Override
     public void invalidateDrawable(final Drawable who) {
-        final Iterator<ViewWeakReference> iterator = mViewSet.iterator();
-        while (iterator.hasNext()) {
-            final View view = iterator.next().get();
+        for (int i = 0; i < mViewList.size(); i++) {
+            final ViewWeakReference reference = mViewList.get(i);
+            final View view = reference.get();
             if (view != null) {
                 view.invalidate();
             } else {
                 // Always remove null references to reduce Set size
-                iterator.remove();
+                mViewList.remove(reference);
             }
         }
     }
 
     @Override
     public void scheduleDrawable(final Drawable who, final Runnable what, final long when) {
-        final Iterator<ViewWeakReference> iterator = mViewSet.iterator();
-        while (iterator.hasNext()) {
-            final View view = iterator.next().get();
+        for (int i = 0; i < mViewList.size(); i++) {
+            final ViewWeakReference reference = mViewList.get(i);
+            final View view = reference.get();
             if (view != null) {
                 view.scheduleDrawable(who, what, when);
             } else {
                 // Always remove null references to reduce Set size
-                iterator.remove();
+                mViewList.remove(reference);
             }
         }
     }
 
     @Override
     public void unscheduleDrawable(final Drawable who, final Runnable what) {
-        final Iterator<ViewWeakReference> iterator = mViewSet.iterator();
-        while (iterator.hasNext()) {
-            final View view = iterator.next().get();
+        for (int i = 0; i < mViewList.size(); i++) {
+            final ViewWeakReference reference = mViewList.get(i);
+            final View view = reference.get();
             if (view != null) {
-                view.unscheduleDrawable(who, what);
+                view.unscheduleDrawable(who);
             } else {
                 // Always remove null references to reduce Set size
-                iterator.remove();
+                mViewList.remove(reference);
             }
         }
     }
 
     /**
      * Associates given {@link View}. If view has been already added, nothing happens.
+     *
      * @param view View to be associated
      */
+
     public void addView(@NonNull final View view) {
-        final Iterator<ViewWeakReference> iterator = mViewSet.iterator();
-        while (iterator.hasNext()) {
-            final View item = iterator.next().get();
+        for (int i = 0; i < mViewList.size(); i++) {
+            final ViewWeakReference reference = mViewList.get(i);
+            final View item = reference.get();
             if (item == null) {
                 // Always remove null references to reduce Set size
-                iterator.remove();
-            } else if (item == view) {
-                // Return when found or if loop end reached, add it.
-                return;
+                mViewList.remove(reference);
             }
         }
-        mViewSet.add(new ViewWeakReference(view));
+        mViewList.addIfAbsent(new ViewWeakReference(view));
     }
 
     /**
      * Disassociates given {@link View}. If view is not associated, nothing happens.
+     *
      * @param view View to be disassociated
      */
     public void removeView(final View view) {
-        final Iterator<ViewWeakReference> iterator = mViewSet.iterator();
-        while (iterator.hasNext()) {
-            final View item = iterator.next().get();
+        for (int i = 0; i < mViewList.size(); i++) {
+            final ViewWeakReference reference = mViewList.get(i);
+            final View item = reference.get();
             if (item == null || item == view) {
                 // Always remove null references to reduce Set size
-                iterator.remove();
+                mViewList.remove(reference);
             }
         }
     }
 
-    private static final class ViewWeakReference extends WeakReference<View>{
+    private static final class ViewWeakReference extends WeakReference<View> {
         ViewWeakReference(View r) {
             super(r);
         }
