5edbb89188f3ac61f3e591494599450bf67d48e3
Karol Wrótniak <koral--@users.noreply.github.com>
2015-06-09 03:42:49 +0200

    splitted modules for #173 begin

---
+++ src/main/jni/Android.mk
@@ -0,0 +1,39 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := pl_droidsonroids_gif
+LOCAL_CFLAGS := -Ofast -DDEBUG
+LOCAL_LDLIBS := \
+	-ljnigraphics \
+	-llog \
+
+LOCAL_SRC_FILES := \
+	drawing.c \
+	gif.c \
+	metadata.c \
+	memset32_neon.S \
+	bitmap.c \
+	open_close.c \
+	decoding.c \
+	exception.c \
+	time.c \
+	control.c \
+	memset.arm.S \
+	giflib/dgif_lib.c \
+	giflib/gifalloc.c \
+
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := pl_droidsonroids_gif_surface
+LOCAL_CFLAGS := -Ofast -DDEBUG
+LOCAL_LDLIBS := \
+	-landroid \
+	-llog \
+
+LOCAL_SRC_FILES := \
+	surface.c \
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ src/main/jni/Application.mk
@@ -0,0 +1,5 @@
+APP_ABI := all
+APP_PLATFORM := android-21
+APP_OPTIM := debug
+
+#NDK_TOOLCHAIN_VERSION := clang
