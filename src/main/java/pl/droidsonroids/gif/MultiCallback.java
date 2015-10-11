e8719ad4f58048e0c37bad9360668398d4370632
Karol Wrótniak <koral--@users.noreply.github.com>
2015-10-11 02:13:30 +0200

    #215 done

--- src/main/java/pl/droidsonroids/gif/MultiCallback.java
+++ src/main/java/pl/droidsonroids/gif/MultiCallback.java
@@ -2,120 +2,120 @@ package pl.droidsonroids.gif;
 
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Drawable.Callback;
-import android.support.annotation.NonNull;
 import android.view.View;
 
 import java.lang.ref.WeakReference;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
- * {@link Callback} which allows single {@link Drawable} to be associated with multiple {@link View}s.
- * If drawable needs to be redrawn all its views are invalidated.
+ * {@link Callback} which allows single {@link Drawable} to be associated with multiple callbacks,
+ * eg. with multiple {@link View}s.
+ * If drawable needs to be redrawn all currently associated callbacks are invoked.
+ * If Callback is a View it is then invalidated.
  *
  * @author koral--
  * @author Doctoror
  */
 public class MultiCallback implements Callback {
 
-    private final CopyOnWriteArrayList<ViewWeakReference> mViewList = new CopyOnWriteArrayList<>();
+	private final CopyOnWriteArrayList<CallbackWeakReference> mCallbacks = new CopyOnWriteArrayList<>();
 
-    @Override
-    public void invalidateDrawable(final Drawable who) {
-        for (int i = 0; i < mViewList.size(); i++) {
-            final ViewWeakReference reference = mViewList.get(i);
-            final View view = reference.get();
-            if (view != null) {
-                view.invalidateDrawable(who);
-            } else {
-                // Always remove null references to reduce Set size
-                mViewList.remove(reference);
-            }
-        }
-    }
+	@Override
+	public void invalidateDrawable(final Drawable who) {
+		for (int i = 0; i < mCallbacks.size(); i++) {
+			final CallbackWeakReference reference = mCallbacks.get(i);
+			final Callback callback = reference.get();
+			if (callback != null) {
+				callback.invalidateDrawable(who);
+			} else {
+				// Always remove null references to reduce list size
+				mCallbacks.remove(reference);
+			}
+		}
+	}
 
-    @Override
-    public void scheduleDrawable(final Drawable who, final Runnable what, final long when) {
-        for (int i = 0; i < mViewList.size(); i++) {
-            final ViewWeakReference reference = mViewList.get(i);
-            final View view = reference.get();
-            if (view != null) {
-                view.scheduleDrawable(who, what, when);
-            } else {
-                // Always remove null references to reduce Set size
-                mViewList.remove(reference);
-            }
-        }
-    }
+	@Override
+	public void scheduleDrawable(final Drawable who, final Runnable what, final long when) {
+		for (int i = 0; i < mCallbacks.size(); i++) {
+			final CallbackWeakReference reference = mCallbacks.get(i);
+			final Callback callback = reference.get();
+			if (callback != null) {
+				callback.scheduleDrawable(who, what, when);
+			} else {
+				// Always remove null references to reduce Set size
+				mCallbacks.remove(reference);
+			}
+		}
+	}
 
-    @Override
-    public void unscheduleDrawable(final Drawable who, final Runnable what) {
-        for (int i = 0; i < mViewList.size(); i++) {
-            final ViewWeakReference reference = mViewList.get(i);
-            final View view = reference.get();
-            if (view != null) {
-                view.unscheduleDrawable(who);
-            } else {
-                // Always remove null references to reduce Set size
-                mViewList.remove(reference);
-            }
-        }
-    }
+	@Override
+	public void unscheduleDrawable(final Drawable who, final Runnable what) {
+		for (int i = 0; i < mCallbacks.size(); i++) {
+			final CallbackWeakReference reference = mCallbacks.get(i);
+			final Callback callback = reference.get();
+			if (callback != null) {
+				callback.unscheduleDrawable(who, what);
+			} else {
+				// Always remove null references to reduce list size
+				mCallbacks.remove(reference);
+			}
+		}
+	}
 
-    /**
-     * Associates given {@link View}. If view has been already added, nothing happens.
-     *
-     * @param view View to be associated
-     */
+	/**
+	 * Associates given {@link Callback}. If callback has been already added, nothing happens.
+	 *
+	 * @param callback Callback to be associated
+	 */
+	public void addView(final Callback callback) {
+		for (int i = 0; i < mCallbacks.size(); i++) {
+			final CallbackWeakReference reference = mCallbacks.get(i);
+			final Callback item = reference.get();
+			if (item == null) {
+				// Always remove null references to reduce list size
+				mCallbacks.remove(reference);
+			}
+		}
+		mCallbacks.addIfAbsent(new CallbackWeakReference(callback));
+	}
 
-    public void addView(@NonNull final View view) {
-        for (int i = 0; i < mViewList.size(); i++) {
-            final ViewWeakReference reference = mViewList.get(i);
-            final View item = reference.get();
-            if (item == null) {
-                // Always remove null references to reduce Set size
-                mViewList.remove(reference);
-            }
-        }
-        mViewList.addIfAbsent(new ViewWeakReference(view));
-    }
+	/**
+	 * Disassociates given {@link Callback}. If callback is not associated, nothing happens.
+	 *
+	 * @param callback Callback to be disassociated
+	 */
+	public void removeView(final Callback callback) {
+		for (int i = 0; i < mCallbacks.size(); i++) {
+			final CallbackWeakReference reference = mCallbacks.get(i);
+			final Callback item = reference.get();
+			if (item == null || item == callback) {
+				// Always remove null references to reduce list size
+				mCallbacks.remove(reference);
+			}
+		}
+	}
 
-    /**
-     * Disassociates given {@link View}. If view is not associated, nothing happens.
-     *
-     * @param view View to be disassociated
-     */
-    public void removeView(final View view) {
-        for (int i = 0; i < mViewList.size(); i++) {
-            final ViewWeakReference reference = mViewList.get(i);
-            final View item = reference.get();
-            if (item == null || item == view) {
-                // Always remove null references to reduce Set size
-                mViewList.remove(reference);
-            }
-        }
-    }
+	private static final class CallbackWeakReference extends WeakReference<Callback> {
+		CallbackWeakReference(Callback r) {
+			super(r);
+		}
 
-    private static final class ViewWeakReference extends WeakReference<View> {
-        ViewWeakReference(View r) {
-            super(r);
-        }
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) {
+				return true;
+			}
+			if (o == null || getClass() != o.getClass()) {
+				return false;
+			}
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
+			return get() == ((CallbackWeakReference) o).get();
+		}
 
-            return get() == ((ViewWeakReference) o).get();
-        }
-
-        @Override
-        public int hashCode() {
-            final View view = get();
-            return view != null ? view.hashCode() : 0;
-        }
-    }
+		@Override
+		public int hashCode() {
+			final Callback callback = get();
+			return callback != null ? callback.hashCode() : 0;
+		}
+	}
 }
