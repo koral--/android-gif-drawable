2f78a2bb7336c53a2eeb1b44b1090d0fd9d89844
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2016-11-02 01:13:44 +0100

    `GifAnimationMetadata#getAllocationByteCount()` javadoc extended

--- android-gif-drawable/src/main/java/pl/droidsonroids/gif/GifAnimationMetaData.java
+++ android-gif-drawable/src/main/java/pl/droidsonroids/gif/GifAnimationMetaData.java
@@ -219,8 +219,11 @@ public class GifAnimationMetaData implements Serializable, Parcelable {
 	 * Like {@link GifDrawable#getAllocationByteCount()} but does not include memory needed for backing {@link android.graphics.Bitmap}.
 	 * {@code Bitmap} in {@code GifDrawable} may be allocated at the time of creation or existing one may be reused if {@link GifDrawableBuilder#with(GifDrawable)}
 	 * is used.
+	 * This method assumes no subsampling (sample size = 1).<br>
+	 * To calculate allocation byte count of {@link GifDrawable} created from the same input source (without bitmap reusing) the following formula can be used:<br>
+	 * {@code (<allocation byte count> + (<width> * <height> * 4)) / <sample size> ^ 2}
 	 *
-	 * @return possible size of the memory needed to store pixels excluding backing {@link android.graphics.Bitmap}
+	 * @return possible size of the memory needed to store pixels excluding backing {@link android.graphics.Bitmap} and assuming no subsampling
 	 */
 	public long getAllocationByteCount() {
 		return mPixelsBytesCount;
