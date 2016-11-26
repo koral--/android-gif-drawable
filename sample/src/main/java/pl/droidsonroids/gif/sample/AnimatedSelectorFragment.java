50d9219fdd28f56a96eda56a5e3495c7eb98be34
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2016-11-27 00:55:34 +0100

    Example for #364 added

---
+++ sample/src/main/java/pl/droidsonroids/gif/sample/AnimatedSelectorFragment.java
@@ -0,0 +1,52 @@
+package pl.droidsonroids.gif.sample;
+
+import android.content.res.XmlResourceParser;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
+import pl.droidsonroids.gif.GifDrawable;
+
+public class AnimatedSelectorFragment extends BaseFragment {
+
+
+	@SuppressWarnings("deprecation")
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		final View rootView = inflater.inflate(R.layout.animated_selector, container, false);
+		final View buttonJava = rootView.findViewById(R.id.button_java);
+		buttonJava.setBackgroundDrawable(getJavaAnimatedBackground());
+
+		final View buttonXML = rootView.findViewById(R.id.button_xml);
+		try {
+			buttonXML.setBackgroundDrawable(getXMLAnimatedBackground());
+		} catch (XmlPullParserException | IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		return rootView;
+	}
+
+	@SuppressWarnings("ResourceType")
+	private Drawable getXMLAnimatedBackground() throws XmlPullParserException, IOException {
+		final XmlResourceParser resourceParser = getResources().getXml(R.drawable.selector);
+		return AnimatedSelectorDrawableGenerator.getDrawable(getResources(), resourceParser);
+	}
+
+	private Drawable getJavaAnimatedBackground() {
+		final StateListDrawable stateListDrawable = new StateListDrawable();
+		stateListDrawable.addState(new int[]{android.R.attr.state_pressed}, GifDrawable.createFromResource(getResources(), R.drawable.anim_flag_chile));
+		stateListDrawable.addState(new int[0], GifDrawable.createFromResource(getResources(), R.drawable.anim_flag_england));
+		return stateListDrawable;
+	}
+
+}
--- sample/src/main/java/pl/droidsonroids/gif/sample/MainPagerAdapter.java
+++ sample/src/main/java/pl/droidsonroids/gif/sample/MainPagerAdapter.java
@@ -8,36 +8,28 @@ import pl.droidsonroids.gif.sample.sources.GifSourcesFragment;
 
 class MainPagerAdapter extends FragmentStatePagerAdapter {
 	private final String[] mPageTitles;
+	private final Fragment[] mPages;
 
 	MainPagerAdapter(FragmentActivity act) {
 		super(act.getSupportFragmentManager());
 		mPageTitles = act.getResources().getStringArray(R.array.pages);
+		mPages = new Fragment[]{
+				new GifSourcesFragment(),
+				new GifTextViewFragment(),
+				new GifTextureFragment(),
+				new ImageSpanFragment(),
+				new AnimationControlFragment(),
+				new HttpFragment(),
+				new TexturePlaceholderFragment(),
+				new GifTexImage2DFragment(),
+				new AnimatedSelectorFragment(),
+				new AboutFragment()
+		};
 	}
 
 	@Override
 	public Fragment getItem(int position) {
-		switch (position) {
-			case 0:
-				return new GifSourcesFragment();
-			case 1:
-				return new GifTextViewFragment();
-			case 2:
-				return new GifTextureFragment();
-			case 3:
-				return new ImageSpanFragment();
-			case 4:
-				return new AnimationControlFragment();
-			case 5:
-				return new HttpFragment();
-			case 6:
-				return new TexturePlaceholderFragment();
-			case 7:
-				return new GifTexImage2DFragment();
-			case 8:
-				return new AboutFragment();
-			default:
-				throw new IndexOutOfBoundsException("Invalid page index");
-		}
+		return mPages[position];
 	}
 
 	@Override
