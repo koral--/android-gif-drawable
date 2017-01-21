dbb10809ac9cf570e0bdfb398dba559cdad90235
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-01-22 00:40:05 +0100

    Unneeded debugging symbols removed, #383 fixed

--- android-gif-drawable/src/main/c/Android.mk
+++ android-gif-drawable/src/main/c/Android.mk
@@ -1,18 +1,13 @@
-cflags:= -Wall -Weverything -std=c11
 extra_ldlibs :=
 
 ifeq ($(NDK_DEBUG),1)
-	cflags+= -DDEBUG
 	extra_ldlibs= -llog
-else
-	cflags+= -fvisibility=hidden
 endif
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := pl_droidsonroids_gif
-LOCAL_CFLAGS := $(cflags)
 LOCAL_LDLIBS := -ljnigraphics -landroid -lGLESv2 $(extra_ldlibs)
 
 LOCAL_SRC_FILES := \
