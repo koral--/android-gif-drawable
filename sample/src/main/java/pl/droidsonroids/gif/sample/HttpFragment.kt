6b94ff2726c9e0937b0d6fdde40781abb3051952
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-11-05 01:08:06 +0100

    Minimum SDK version increased to 14

--- sample/src/main/java/pl/droidsonroids/gif/sample/HttpFragment.kt
+++ sample/src/main/java/pl/droidsonroids/gif/sample/HttpFragment.kt
@@ -1,8 +1,6 @@
 package pl.droidsonroids.gif.sample
 
-import android.os.Build
 import android.os.Bundle
-import android.support.annotation.RequiresApi
 import android.support.design.widget.Snackbar
 import android.view.LayoutInflater
 import android.view.View
@@ -25,7 +23,7 @@ class HttpFragment : BaseFragment(), View.OnClickListener {
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH && !gifTextureView!!.isHardwareAccelerated) {
+		if (!gifTextureView!!.isHardwareAccelerated) {
 			Snackbar.make(gifTextureView!!, R.string.gif_texture_view_stub_acceleration, Snackbar.LENGTH_LONG).show()
 		}
 	}
@@ -35,11 +33,9 @@ class HttpFragment : BaseFragment(), View.OnClickListener {
 		super.onDestroy()
 	}
 
-	@RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 	internal fun onGifDownloaded(buffer: ByteBuffer) =
 			gifTextureView!!.setInputSource(InputSource.DirectByteBufferSource(buffer))
 
-	@RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 	internal fun onDownloadFailed(e: Exception) {
 		gifTextureView!!.setOnClickListener(this@HttpFragment)
 		if (isDetached) {
@@ -50,11 +46,9 @@ class HttpFragment : BaseFragment(), View.OnClickListener {
 
 	}
 
-	@RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 	private fun downloadGif() {
 		executorService.submit(GifLoadTask(this))
 	}
 
-	@RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 	override fun onClick(v: View) = downloadGif()
 }
