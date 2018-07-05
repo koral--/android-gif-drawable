9f8e75732ca5cafd9ecaec1bca62f6e1e4e3b0ea
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2018-07-05 02:11:36 +0200

    Refactor live wallpaper scaling

--- sample/src/main/java/pl/droidsonroids/gif/sample/opengl/GifTexImage2DProgram.kt
+++ sample/src/main/java/pl/droidsonroids/gif/sample/opengl/GifTexImage2DProgram.kt
@@ -1,7 +1,10 @@
 package pl.droidsonroids.gif.sample.opengl
 
 import android.opengl.GLES20.*
-import android.opengl.Matrix
+import android.widget.ImageView
+import android.widget.ImageView.ScaleType.FIT_CENTER
+import android.widget.ImageView.ScaleType.FIT_XY
+import org.intellij.lang.annotations.Language
 import pl.droidsonroids.gif.GifTexImage2D
 
 class GifTexImage2DProgram(private val gifTexImage2D: GifTexImage2D) {
@@ -9,25 +12,23 @@ class GifTexImage2DProgram(private val gifTexImage2D: GifTexImage2D) {
     val width = gifTexImage2D.width
     val currentFrameDuration = gifTexImage2D.getFrameDuration(gifTexImage2D.currentFrameIndex)
 
-    private var texMatrixLocation = -1
-    private val texMatrix = FloatArray(16)
     private var program = 0
     private var positionLocation = -1
     private var textureLocation = -1
     private var coordinateLocation = -1
 
+    @Language("GLSL")
     private val vertexShaderCode = """
         attribute vec4 position;
-        uniform mediump mat4 texMatrix;
         attribute vec4 coordinate;
         varying vec2 textureCoordinate;
         void main() {
             gl_Position = position;
-            mediump vec4 outCoordinate = texMatrix * coordinate;
-            textureCoordinate = vec2(outCoordinate.s, 1.0 - outCoordinate.t);
+            textureCoordinate = vec2(coordinate.s, 1.0 - coordinate.t);
         }
         """
 
+    @Language("GLSL")
     private val fragmentShaderCode = """
         varying mediump vec2 textureCoordinate;
         uniform sampler2D texture;
@@ -57,7 +58,6 @@ class GifTexImage2DProgram(private val gifTexImage2D: GifTexImage2D) {
         glDeleteShader(vertexShader)
         positionLocation = glGetAttribLocation(program, "position")
         textureLocation = glGetUniformLocation(program, "texture")
-        texMatrixLocation = glGetUniformLocation(program, "texMatrix")
         coordinateLocation = glGetAttribLocation(program, "coordinate")
 
         glActiveTexture(GL_TEXTURE0)
@@ -70,19 +70,47 @@ class GifTexImage2DProgram(private val gifTexImage2D: GifTexImage2D) {
         glVertexAttribPointer(positionLocation, 2, GL_FLOAT, false, 0, verticesBuffer)
     }
 
-    fun setDimensions(width: Int, height: Int) {
-        val scaleX = width.toFloat() / gifTexImage2D.width
-        val scaleY = height.toFloat() / gifTexImage2D.height
-        Matrix.setIdentityM(texMatrix, 0)
-        Matrix.scaleM(texMatrix, 0, scaleX, scaleY, 1f)
-        Matrix.translateM(texMatrix, 0, 1 / scaleX / 2 - 0.5f, 1 / scaleY / 2 - 0.5f, 0f)
-        glUniformMatrix4fv(texMatrixLocation, 1, false, texMatrix, 0)
+    private var viewport = Viewport(0, 0, 0, 0)
+
+    fun setDimensions(screenWidth: Int, screenHeight: Int, scaleType: ImageView.ScaleType = FIT_CENTER) {
+        viewport = when (scaleType) {
+            FIT_XY -> Viewport(0, 0, screenWidth, screenHeight)
+            FIT_CENTER -> createFitCenterViewport(screenWidth, screenHeight)
+            else -> TODO("$scaleType is not yet supported")
+        }
+    }
+
+    private fun createFitCenterViewport(screenWidth: Int, screenHeight: Int): Viewport {
+        val screenAspectRatio = screenWidth / screenHeight.toFloat()
+        val mediaAspectRation = gifTexImage2D.width / gifTexImage2D.height.toFloat()
+
+        val scaleFactor = if (screenAspectRatio < mediaAspectRation) {
+            screenWidth / gifTexImage2D.width.toFloat()
+        } else {
+            screenHeight / gifTexImage2D.height.toFloat()
+        }
+
+        val viewportWidth = (gifTexImage2D.width * scaleFactor).toInt()
+        val viewportHeight = (gifTexImage2D.height * scaleFactor).toInt()
+
+        return Viewport(
+            (screenWidth - viewportWidth) / 2,
+            (screenHeight - viewportHeight) / 2,
+            viewportWidth, viewportHeight
+        )
     }
 
     fun draw() {
+        viewport.applyToGlWindow()
         gifTexImage2D.glTexSubImage2D(GL_TEXTURE_2D, 0)
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
     }
 
     fun destroy() = gifTexImage2D.recycle()
+}
+
+class Viewport(private val offsetX: Int, private val offsetY: Int, private val width: Int, private val height: Int) {
+    fun applyToGlWindow() {
+        glViewport(offsetX, offsetY, width, height)
+    }
 }
