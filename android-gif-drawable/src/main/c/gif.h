da243e175ff1373726b78137465c20007689ac5d
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2018-11-03 18:48:30 +0100

    Remove unneeded macros. Fix Android test runner dependency.

--- android-gif-drawable/src/main/c/gif.h
+++ android-gif-drawable/src/main/c/gif.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#define _GNU_SOURCE 1
 #ifdef __clang__
 #pragma clang system_header
 #pragma clang diagnostic ignored "-Wgnu"
@@ -34,12 +33,6 @@
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 #endif
 
-#define TEMP_FAILURE_RETRY(exp) ({         \
-    __typeof__(exp) _rc;                   \
-    do {                                   \
-        _rc = (exp);                       \
-    } while (_rc == -1 && errno == EINTR); \
-    _rc; })
 #define THROW_ON_NONZERO_RESULT(fun, message) if ((fun) !=0) throwException(env, RUNTIME_EXCEPTION_ERRNO, message)
 #define GET_ADDR(bm, width, left, top) ((bm) + (top) * (width) + (left))
 
