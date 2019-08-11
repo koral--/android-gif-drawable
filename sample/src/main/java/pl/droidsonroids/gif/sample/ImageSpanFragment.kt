6ac3208b13e8836b64639a4c76f32312d82e1c06
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2019-08-11 03:55:22 +0200

    Fix view leak in ImageSpanFragment

--- sample/src/main/java/pl/droidsonroids/gif/sample/ImageSpanFragment.kt
+++ sample/src/main/java/pl/droidsonroids/gif/sample/ImageSpanFragment.kt
@@ -7,35 +7,34 @@ import android.text.style.ImageSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.image_span.*
 import pl.droidsonroids.gif.GifDrawable
 
 class ImageSpanFragment : Fragment(), Drawable.Callback {
 
-	private lateinit var textView: TextView
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        inflater.inflate(R.layout.image_span, container, false)
 
-	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-		textView = TextView(activity)
-		textView.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
-		textView.setTextIsSelectable(true)
-		val gifDrawable = GifDrawable.createFromResource(resources, R.drawable.anim_flag_england)
-		val ssb = SpannableStringBuilder("test\ufffc")
-		gifDrawable!!.setBounds(0, 0, gifDrawable.intrinsicWidth, gifDrawable.intrinsicHeight)
-		gifDrawable.callback = this
-		ssb.setSpan(ImageSpan(gifDrawable), ssb.length - 1, ssb.length, 0)
-		textView.text = ssb
-		return textView
-	}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        imageSpanTextView.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+        val gifDrawable = GifDrawable.createFromResource(resources, R.drawable.anim_flag_england)
+        val stringBuilder = SpannableStringBuilder("test\ufffc")
+        gifDrawable!!.setBounds(0, 0, gifDrawable.intrinsicWidth, gifDrawable.intrinsicHeight)
+        gifDrawable.callback = this
+        stringBuilder.setSpan(ImageSpan(gifDrawable), stringBuilder.length - 1, stringBuilder.length, 0)
+        imageSpanTextView.text = stringBuilder
+    }
 
-	override fun invalidateDrawable(who: Drawable) =
-			textView.invalidate()
+    override fun invalidateDrawable(who: Drawable) {
+        imageSpanTextView?.invalidate()
+    }
 
-	override fun scheduleDrawable(who: Drawable, what: Runnable, `when`: Long) {
-		textView.postDelayed(what, `when`)
-	}
+    override fun scheduleDrawable(who: Drawable, what: Runnable, `when`: Long) {
+        imageSpanTextView?.postDelayed(what, `when`)
+    }
 
-	override fun unscheduleDrawable(who: Drawable, what: Runnable) {
-		textView.removeCallbacks(what)
-	}
+    override fun unscheduleDrawable(who: Drawable, what: Runnable) {
+        imageSpanTextView?.removeCallbacks(what)
+    }
 }
