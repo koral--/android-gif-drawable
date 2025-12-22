fd41a76379dc37308e803801383cd8f2264f9638
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2025-12-22 18:28:19 +0100

    Remove extra file descriptor close in openNativeFileDescriptor
    Fixes #895

--- android-gif-drawable/src/main/c/gif.c
+++ android-gif-drawable/src/main/c/gif.c
@@ -353,7 +353,6 @@ Java_pl_droidsonroids_gif_GifInfoHandle_openNativeFileDescriptor(JNIEnv *env, jc
 		GifInfo *const info = createGifInfoFromFile(env, file, sourceLength);
 		if (info == NULL) {
 			fclose(file);
-			close(fd);
 		}
 		return (jlong) info;
 	} else {
