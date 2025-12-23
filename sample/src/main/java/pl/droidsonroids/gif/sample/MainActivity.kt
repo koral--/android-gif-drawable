daf3654215cadb0d45f4c1e0e3220ff2cd7d75e5
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2025-12-23 16:18:04 +0100

    Update dependency versions

--- sample/src/main/java/pl/droidsonroids/gif/sample/MainActivity.kt
+++ sample/src/main/java/pl/droidsonroids/gif/sample/MainActivity.kt
@@ -1,6 +1,8 @@
 package pl.droidsonroids.gif.sample
 
 import android.os.Bundle
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
@@ -16,6 +18,12 @@ class MainActivity : FragmentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
 		val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
 		val pager = findViewById<ViewPager2>(R.id.mainViewPager)
